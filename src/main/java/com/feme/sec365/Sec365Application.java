723658d884d5d95ca4a349888d7224666c22292a
RockVoid <devroque18@gmail.com>
2025-05-06 09:28:20 -0300

    [PROJECT SETUP]

---
+++ src/main/java/com/feme/sec365/Sec365Application.java
@@ -0,0 +1,13 @@
+package com.feme.sec365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sec365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sec365Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/feme/sec365/Sec365ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feme.sec365;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sec365ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
